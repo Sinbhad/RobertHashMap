34b8f8bf536c8d9d64c0eb49bf705e3aef1c6e3b
Sinbhad <157553879+Sinbhad@users.noreply.github.com>
2026-04-10 13:24:43 -0400

    Fixes and improvements

--- src/RobertHashMap.java
+++ src/RobertHashMap.java
@@ -8,46 +8,80 @@ public class RobertHashMap {
     public RobertHashMap() {
         data = new RobertLinkedList[5];
         this.size = 0;
+        this.collisions = 0;
     }
 
-    private int simpleHashFunction(String key){
-        return key.length();
+    private int simpleHashFunction(String key, int capacity) {
+        return key.length() % capacity;
     }
 
-    private void put(String key, String value){
-        int hashIndex = simpleHashFunction(key);
-        RobertLinkedList temp = data[hashIndex];
-        if(temp != null){
-            collisions++;
+    public void put(String key, String value) {
+        if ((double) size / data.length >= 0.75) {
+            dynamicResize();
         }
-        temp.add(key, value);
+
+        int hashIndex = simpleHashFunction(key, data.length);
+        if (data[hashIndex] == null) {
+            data[hashIndex] = new RobertLinkedList();
+        } else {
+            this.collisions++;
+        }
+        data[hashIndex].add(key, value);
         size++;
     }
 
-    private void removeKey(String key){
-        int  hashIndex = simpleHashFunction(key);
-        RobertLinkedList temp = data[hashIndex];
-        temp.remove(key);
-        size--;
-    }
-
-    private Boolean containsKey(String key){
-        int hashIndex = simpleHashFunction(key);
-        RobertLinkedList temp = data[hashIndex];
-        return temp.findKey(key);
-    }
-
-    private Boolean containsValue(String value){
-        RobertLinkedList temp = new RobertLinkedList();
-        boolean found = false;
-        for(int i = 0; i > data.length; i++){
-            temp = data[i];
-            found = temp.findValue(value);
+    public void removeKey(String key) {
+        int hashIndex = simpleHashFunction(key, data.length);
+        RobertLinkedList list = data[hashIndex];
+        if (list != null && list.findKey(key)) {
+            list.remove(key);
+            size--;
         }
-        return found;
     }
 
-    private int size(){
+    public boolean containsKey(String key) {
+        int hashIndex = simpleHashFunction(key, data.length);
+        RobertLinkedList list = data[hashIndex];
+        return list != null && list.findKey(key);
+    }
+
+    public boolean containsValue(String value) {
+        for (RobertLinkedList list : data) {
+            if (list != null && list.findValue(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getSize() {
         return size;
+    }
+
+    public int getCollisions() {
+        return collisions;
+    }
+
+    private void dynamicResize() {
+        int newCapacity = data.length * 2;
+        RobertLinkedList[] newData = new RobertLinkedList[newCapacity];
+        this.collisions = 0;
+
+        for (RobertLinkedList list : data) {
+            if (list != null) {
+                Node current = list.getHead();
+                while (current != null) {
+                    int newIndex = simpleHashFunction(current.getKey(), newCapacity);
+                    if (newData[newIndex] == null) {
+                        newData[newIndex] = new RobertLinkedList();
+                    } else {
+                        this.collisions++;
+                    }
+                    newData[newIndex].add(current.getKey(), current.getValue());
+                    current = current.getNextNode();
+                }
+            }
+        }
+        this.data = newData;
     }
 }
--- src/lib/RobertLinkedList.java
+++ src/lib/RobertLinkedList.java
@@ -9,7 +9,7 @@ public class RobertLinkedList {
             head = newNode;
         } else {
             Node tempNode = head;
-            while(tempNode.getNextNode() != null){
+            while (tempNode.getNextNode() != null) {
                 tempNode = tempNode.getNextNode();
             }
             tempNode.setNextNode(newNode);
@@ -17,100 +17,59 @@ public class RobertLinkedList {
         }
     }
 
-
-
-    public void printAll(){
+    public void printAll() {
         Node tempNode = head;
-        while(tempNode != null){
+        while (tempNode != null) {
             System.out.println(tempNode.getValue());
             tempNode = tempNode.getNextNode();
         }
     }
 
-    public void printReverse(){
-        Node tempNode = head.getLast();
-        while(tempNode != null){
-            System.out.println(tempNode.getValue());
-            tempNode = tempNode.getLastNode();
-        }
-    }
-
-
-    public void removeAt(int index) {
+    public boolean findKey(String key) {
         Node tempNode = head;
-
-        if (index == 0) {
-            Node newNextNode = tempNode.getNextNode();
-            if (newNextNode != null) {
-                newNextNode.setLastNode(null);
+        while (tempNode != null) {
+            if (tempNode.getKey().equals(key)) {
+                return true;
             }
-            head = newNextNode;
-        } else {
-                for (int i = 0; i < index; i++) {
-                    tempNode = tempNode.getNextNode();
-                }
-                Node newNode = new Node();
-                tempNode.setNextNode(newNode);
-                tempNode.setLastNode(tempNode);
-        }
-    }
-
-    public Boolean findKey(String key) {
-        Node tempNode = head;
-        if(tempNode.getKey().equals(key)){
-            return true;
-        }else{
-            while(tempNode.checkIfNextExists() && !tempNode.getKey().equals(key)){
-                tempNode = tempNode.getNextNode();
-                if(tempNode.equals(key)){
-                    return true;
-                }
-            }
+            tempNode = tempNode.getNextNode();
         }
         return false;
     }
 
-    public Boolean findValue(String value) {
+    public boolean findValue(String value) {
         Node tempNode = head;
-        if(tempNode.getValue().equals(value)){
-            return true;
-        }else{
-            while(tempNode.checkIfNextExists() && !tempNode.getValue().equals(value)){
-                tempNode = tempNode.getNextNode();
-                if(tempNode.equals(value)){
-                    return true;
-                }
+        while (tempNode != null) {
+            if (tempNode.getValue().equals(value)) {
+                return true;
             }
+            tempNode = tempNode.getNextNode();
         }
         return false;
     }
 
-    public void remove(String value) {
+    public Node getHead() {
+        return head;
+    }
+
+    public void remove(String key) {
         Node tempNode = head;
-        boolean found = false;
+        while (tempNode != null) {
+            if (tempNode.getKey().equals(key)) {
+                Node prev = tempNode.getLastNode();
+                Node next = tempNode.getNextNode();
 
-        if (head.getValue().equalsIgnoreCase(value)) {
-            Node newNextNode = tempNode.getNextNode();
-
-            if (newNextNode != null) {
-                newNextNode.setLastNode(null);
-            }
-            head = newNextNode;
-
-        } else {
-            while (tempNode != null && !found) {
-                if (tempNode.getValue().equalsIgnoreCase(value)) {
-                    Node newNode = new Node();
-                    tempNode.setNextNode(newNode);
-                    tempNode.setLastNode(tempNode);
-                    found = true;
+                if (prev != null) {
+                    prev.setNextNode(next);
                 } else {
-                    tempNode = tempNode.getNextNode();
+                    head = next;
                 }
+
+                if (next != null) {
+                    next.setLastNode(prev);
+                }
+                return;
             }
-        }
-        if(!found){
-            System.out.println("The given value '" + value + "' does not exist in the linked list\n\n");
+            tempNode = tempNode.getNextNode();
         }
     }
 }
